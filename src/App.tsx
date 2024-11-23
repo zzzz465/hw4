6f008ca51f16481a230dada17e5062069ade02b4
jungooji <zzzz465@naver.com>
2024-11-23 17:39:42 +0900

    fix UI not visible

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Layout } from './components/layout/Layout';
-import { RequireAuth } from './components/auth/RequireAuth';
-import Home from './pages/home/Home';
-import Popular from './pages/popular/Popular';
-import Search from './pages/search/Search';
-import Wishlist from './pages/wishlist/Wishlist';
-import MovieDetail from './pages/movie/MovieDetail';
-import SignIn from './pages/auth/SignIn';
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
+import { Layout } from './components/layout/Layout'
+import { RequireAuth } from './components/auth/RequireAuth'
+import Home from './pages/home/Home'
+import Popular from './pages/popular/Popular'
+import Search from './pages/search/Search'
+import Wishlist from './pages/wishlist/Wishlist'
+import MovieDetail from './pages/movie/MovieDetail'
+import SignIn from './pages/auth/SignIn'
+import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/signin" element={<SignIn />} />
+    <MantineProvider>
+      <Notifications position='bottom-right' zIndex={1000} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/signin" element={<SignIn />} />
 
-      <Route element={<RequireAuth><Layout /></RequireAuth>}>
-        <Route index element={<Home />} />
-        <Route path="popular" element={<Popular />} />
-        <Route path="search" element={<Search />} />
-        <Route path="wishlist" element={<Wishlist />} />
-        <Route path="movies/:id" element={<MovieDetail />} />
-      </Route>
-
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+          <Route element={<RequireAuth><Layout /></RequireAuth>}>
+            <Route index element={<Home />} />
+            <Route path="popular" element={<Popular />} />
+            <Route path="search" element={<Search />} />
+            <Route path="wishlist" element={<Wishlist />} />
+            <Route path="movies/:id" element={<MovieDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  )
 }
 
 
